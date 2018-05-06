c632e3893ec6f64c717ed95b344479f87413da43
Aniket Aryamane <aryamane.aniket@gmail.com>
2018-05-06 20:20:19 +0530

    Added class based view - Aniket

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Movie
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'core/index.html', {})
+class MovieListView(ListView):
+    model = Movie
+    template_name = 'core/index.html'
+    context_object_name = 'movies'
+    # ordering = ['year']
